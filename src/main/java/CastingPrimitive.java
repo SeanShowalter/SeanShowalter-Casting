c7672a4e469c31d31bed21ea459dba6920bdec03
Sean Showalter <sean@showalter.design>
2023-02-28 07:02:10 +0000

    Tests passed.

--- src/main/java/CastingPrimitive.java
+++ src/main/java/CastingPrimitive.java
@@ -3,7 +3,7 @@ public class CastingPrimitive {
     /**
      * Casting is the conversion of one type to another. It exists for both primitive and object types.
      * Casting a double, which is a number with decimals, to an int, which is a number without decimals, will eliminate
-     * the decimal portion of the number.
+     * the decimal portion of the number. 
      *
      * If you try to just return d without changes, the method will not compile because d needs to be converted
      * to an int first to reflect the int return type.
